8bf2bc578cb7bd8442128487d4cd4c1935ac2e42
Matan Maron <38442407+matanmaron@users.noreply.github.com>
2021-12-04 15:14:42 +0200

    l8 up to part 15 ex

---
+++ Assets/L8/PlayerScript.cs
@@ -0,0 +1,126 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace L8
+{
+    public class PlayerScript : NetworkBehaviour
+    {
+        public TextMesh playerNameText;
+        public GameObject floatingInfo;
+
+        private Material playerMaterialClone;
+        private SceneScript sceneScript;
+
+        private int selectedWeaponLocal = 1;
+        public GameObject[] weaponArray;
+
+        [SyncVar(hook = nameof(OnWeaponChanged))]
+        public int activeWeaponSynced = 1;
+
+        [SyncVar(hook = nameof(OnNameChanged))]
+        public string playerName;
+
+        [SyncVar(hook = nameof(OnColorChanged))]
+        public Color playerColor = Color.white;
+
+        void OnNameChanged(string _Old, string _New)
+        {
+            playerNameText.text = playerName;
+        }
+
+        void OnColorChanged(Color _Old, Color _New)
+        {
+            playerNameText.color = _New;
+            playerMaterialClone = new Material(GetComponent<Renderer>().material);
+            playerMaterialClone.color = _New;
+            GetComponent<Renderer>().material = playerMaterialClone;
+        }
+
+        void Awake()
+        {
+            //allow all players to run this
+            sceneScript = GameObject.FindObjectOfType<SceneScript>();
+            // disable all weapons
+            foreach (var item in weaponArray)
+                if (item != null)
+                    item.SetActive(false);
+        }
+
+        void OnWeaponChanged(int _Old, int _New)
+        {
+            // disable old weapon
+            // in range and not null
+            if (0 < _Old && _Old < weaponArray.Length && weaponArray[_Old] != null)
+                weaponArray[_Old].SetActive(false);
+
+            // enable new weapon
+            // in range and not null
+            if (0 < _New && _New < weaponArray.Length && weaponArray[_New] != null)
+                weaponArray[_New].SetActive(true);
+        }
+
+        [Command]
+        public void CmdChangeActiveWeapon(int newIndex)
+        {
+            activeWeaponSynced = newIndex;
+        }
+
+        [Command]
+        public void CmdSendPlayerMessage()
+        {
+            if (sceneScript)
+                sceneScript.statusText = $"{playerName} says hello {Random.Range(10, 99)}";
+        }
+
+        [Command]
+        public void CmdSetupPlayer(string _name, Color _col)
+        {
+            //player info sent to server, then server updates sync vars which handles it on all clients
+            playerName = _name;
+            playerColor = _col;
+            sceneScript.statusText = $"{playerName} joined.";
+        }
+
+        public override void OnStartLocalPlayer()
+        {
+            sceneScript.playerScript = this;
+            Camera.main.transform.SetParent(transform);
+            Camera.main.transform.localPosition = new Vector3(0, 0, 0);
+
+            floatingInfo.transform.localPosition = new Vector3(0, -0.3f, 0.6f);
+            floatingInfo.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+            string name = "Player" + Random.Range(100, 999);
+            Color color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+            CmdSetupPlayer(name, color);
+        }
+
+        void Update()
+        {
+            if (!isLocalPlayer)
+            {
+                // make non-local players run this
+                floatingInfo.transform.LookAt(Camera.main.transform);
+                return;
+            }
+
+            float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * 110.0f;
+            float moveZ = Input.GetAxis("Vertical") * Time.deltaTime * 4f;
+
+            transform.Rotate(0, moveX, 0);
+            transform.Translate(0, 0, moveZ);
+
+            if (Input.GetButtonDown("Fire2")) //Fire2 is mouse 2nd click and left alt
+            {
+                selectedWeaponLocal += 1;
+
+                if (selectedWeaponLocal > weaponArray.Length)
+                    selectedWeaponLocal = 1;
+
+                CmdChangeActiveWeapon(selectedWeaponLocal);
+            }
+        }
+    }
+}
---
+++ Assets/L8/SceneScript.cs
@@ -0,0 +1,29 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace L8
+{
+    public class SceneScript : NetworkBehaviour
+    {
+        public Text canvasStatusText;
+        public PlayerScript playerScript;
+
+        [SyncVar(hook = nameof(OnStatusTextChanged))]
+        public string statusText;
+
+        void OnStatusTextChanged(string _Old, string _New)
+        {
+            //called from sync var hook, to update info on screen for all players
+            canvasStatusText.text = statusText;
+        }
+
+        public void ButtonSendMessage()
+        {
+            if (playerScript != null)
+                playerScript.CmdSendPlayerMessage();
+        }
+    }
+}
