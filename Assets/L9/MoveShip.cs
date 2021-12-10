905a6c68e6e8ca0c5b98a45fa4f3eb022ad8ef20
Matan Maron <38442407+matanmaron@users.noreply.github.com>
2021-12-10 14:04:56 +0200

    space ships part 1

---
+++ Assets/L9/MoveShip.cs
@@ -0,0 +1,20 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace L9
+{
+    public class MoveShip : NetworkBehaviour
+    {
+        [SerializeField] private float speed;
+        void FixedUpdate()
+        {
+            if (this.isLocalPlayer)
+            {
+                float movement = Input.GetAxis("Horizontal");
+                GetComponent<Rigidbody2D>().velocity = new Vector2(movement * speed, 0.0f);
+            }
+        }
+    }
+}
